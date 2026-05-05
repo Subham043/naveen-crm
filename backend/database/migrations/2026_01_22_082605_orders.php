2c7a81002da3ebd1b708f8c1c793f9b560565cfa
Subham043 <sahasubham043@gmail.com>
2026-05-05 19:25:12 +0530

    fixed auto count of order

--- backend/database/migrations/2026_01_22_082605_orders.php
+++ backend/database/migrations/2026_01_22_082605_orders.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -30,6 +31,9 @@ return new class extends Migration
             $table->foreignId('quotation_id')->unique()->nullable()->index();
             $table->timestamps();
         });
+
+        // Set starting AUTO_INCREMENT value
+        DB::statement('ALTER TABLE orders AUTO_INCREMENT = 3879;');
     }
 
     /**
